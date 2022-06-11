a7be746111b4ce43c63fd9e5b0f4dc5b36619b8a
maki <yayu.060842@gmail.com>
2022-06-11 11:03:20 +0000

    グラフ

--- app/Http/Controllers/Admin/WeightGraphController.php
+++ app/Http/Controllers/Admin/WeightGraphController.php
@@ -7,5 +7,14 @@ use Illuminate\Http\Request;
 
 class WeightGraphController extends Controller
 {
-    //
+    function show(Request $request){
+		//体重ログデータを取り出す
+		//今年の体重データを取り出す
+		$log_list = WeightLog::where("date_key","like",date("Y") . "%")->get();
+		
+		//Viewにログデータを渡す
+		return view("weight_graph",[
+			"log_list" => $log_list
+		]);
+	}
 }
---
+++ resources/views/admin/graph/weight_graph.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.admin')
+       
+@section('title','職員情報')
+       
+@section('content')    
+    <div class="container">
+            <div class="header-title-area">
+                <h1>graph</h1>
+                    ここにグラフを表示
+                    {{ $log_list }}
